4ee1dad3c303b67e008c60df3c246e8b088d4e50
Behrouz Roameh <behrouz.rozmeh@gmail.com>
2022-06-08 20:56:34 +0900

    I get data from a fake api

---
+++ src/services/api.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const BASE_URL="https://fakestoreapi.com";
+
+const getProducts= async ()=>{
+    const response =await axios.get (`${BASE_URL}/products`);
+    return response.data;
+}
+export {getProducts};
