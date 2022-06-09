fbce7a78770dbafa45fa3c3fc2e99daf9b9a993e
Behrouz Roameh <behrouz.rozmeh@gmail.com>
2022-06-09 23:51:43 +0900

    I temporary used inline styles

--- src/components/Store.js
+++ src/components/Store.js
@@ -9,7 +9,7 @@ import {ProductsContext} from "../context/ProductContextProvider"
 const Store = () => {
     const products = useContext(ProductsContext)
     return (
-        <div>
+        <div style={{display:"flex", flexWrap:"wrap", justifyContent:"space-between"}}>
 
        {
            products.map(product=><Product
--- src/components/shared/Product.js
+++ src/components/shared/Product.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Product = ({productData}) => {
     return (
         <div>
-            <img src={productData.image} alt="product"/>
+            <img src={productData.image} alt="product" style={{width:"200px"}}/>
             <h3>{productData.title}</h3>
             <p>{productData.price}</p>
             <div>
