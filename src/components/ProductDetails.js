b55ad56a4936ecca39d510c81ff2984cc2a3a635
Behrouz Roameh <behrouz.rozmeh@gmail.com>
2022-06-10 22:46:12 +0900

    Details page are updated

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React,{useContext} from 'react';
+import { Link } from 'react-router-dom';
+
+//context
+import { ProductsContext } from '../context/ProductContextProvider';
+
 
 const ProductDetails = (props) => {
     const id =props.match.params.id;
+    const data=useContext(ProductsContext);
+    const product =data[id-1];
+    const {image, title, description, price, category}=product;
+
     return (
         <div>
-           Details -{id};
+           <img src={image} alt='product'/>
+           <div>
+               <h3>{title}</h3>
+               <p>{description}</p>
+               <p><span>Category:</span>{category}</p>
+               <div>
+                   <span>{price}</span>
+                <Link to={"/products"}>Back to Shop</Link>
+               </div>
+           </div>
         </div>
     );
 };
