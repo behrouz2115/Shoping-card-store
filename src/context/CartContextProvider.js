350cc38d249e87c21d25ebebf0bb7e73baefb269
Behrouz Roameh <behrouz.rozmeh@gmail.com>
2022-06-13 19:25:45 +0900

    ADD_ITEM add items to cart & REMOVE_ITEM remove items from cart

--- src/context/CartContextProvider.js
+++ src/context/CartContextProvider.js
@@ -1,5 +1,30 @@
 import React,{useReducer} from 'react';
 
+const initialstate={
+    selectedItems:[],
+    itemsCounter:0,
+    total:0,
+    chechout:false
+}
+const cartReducer =(state,action)=>{
+    switch(action.type){
+        case "ADD_ITEM":
+            if (!state.selectedItems.find(item=>item.id===action.payload.id)){
+                state.selectedItems.push({
+                    ...action.payload,
+                    quantity:1
+                })
+            }
+            return{
+                ...state,
+                selectedItems:[...state.selectedItems]
+            }
+            case "REMOVE_ITEM":
+                const newSelectedItems=state.selectedItems.filter(items=>items.id!==action.payload.id);
+                selectedItems:[...newSelectedItems]
+    }
+}
+
 const CartContextProvider = () => {
     const [state,disPatch]=useReducer(cartReducer,initialstate)
     return (
