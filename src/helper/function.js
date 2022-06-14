7e3acd65c4c84e1289cfa3fba068f0adf7bce4f2
Behrouz Roameh <behrouz.rozmeh@gmail.com>
2022-06-14 21:14:16 +0900

    function quantityCount is added

--- src/helper/function.js
+++ src/helper/function.js
@@ -7,4 +7,13 @@ const isInCart = (state,id)=>{
     const result=!!state.selectedItems.find(item=>item.id===id);
     return result;
 }
-export {shorten,isInCart};
+const quantityCount = (state,id)=>{
+    const index=state.selectedItems.findIndex(item=>item.id===id);
+    if (index===-1){
+        return false;
+    }else{
+        return state.selectedItems[index].quantity;
+    }
+}
+
+export {shorten,isInCart,quantityCount};
