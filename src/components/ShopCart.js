57c0ad1bcaabe9a3574abcd8d47b99d72d0a523c
Behrouz Roameh <behrouz.rozmeh@gmail.com>
2022-06-15 21:20:09 +0900

    Cart is added

--- src/components/ShopCart.js
+++ src/components/ShopCart.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+//component
+import Cart from './shared/Cart';
+
+//context
+import { CarttContext } from '../context/CartContextProvider';
 
 const ShopCart = () => {
+    const {state,dispatch}=useContext(CarttContext);
     return (
         <div>
             <h1>Shop cart</h1>
+            <div>
+                {state.selectedItems.map(item=><Cart key={item.id} data={item}/>)}
+            </div>
         </div>
     );
 };
---
+++ src/components/shared/Cart.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cart = () => {
+    return (
+        <div>
+            <p>cart</p>
+        </div>
+    );
+};
+
+export default Cart;
