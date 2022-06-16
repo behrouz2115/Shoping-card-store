58572f06c6a37d3ccc3611150ca459244ffc4688
Behrouz Roameh <behrouz.rozmeh@gmail.com>
2022-06-16 22:31:56 +0900

    navbar style is added

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -6,14 +6,19 @@ import { CarttContext } from '../../context/CartContextProvider';
 //icone
 import shopIcon from "../../assets/icons/shop.svg";
 
+//style
+import styles from "./Navbar.module.css";
+
 const Navbar = () => {
     const {state} = useContext(CarttContext);
     return (
-        <div>
-            <Link to="/product">Product</Link>
-            <div>
-                <Link to="/cart"><img src={shopIcon} alt="shop"/></Link>
-                <span>{state.itemsCounter}</span>
+        <div className={styles.mainContainer}>
+            <div className={styles.container}>
+               <Link className={styles.productLink} to="/product">Product</Link>
+               <div className={styles.iconContainer}>
+                   <Link to="/cart"><img className={styles.svg}  src={shopIcon} alt="shop"/></Link>
+                   <span>{state.itemsCounter}</span>
+               </div>
             </div>
             
         </div>
