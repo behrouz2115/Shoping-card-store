eab88bd74618c4aa78aaaf2d0d4a728b57e961bc
Behrouz Roameh <behrouz.rozmeh@gmail.com>
2022-06-16 20:00:30 +0900

    Shop cart updated

--- src/components/ShopCart.js
+++ src/components/ShopCart.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
 
 //component
 import Cart from './shared/Cart';
@@ -13,6 +14,24 @@ const ShopCart = () => {
             <div>
                 {state.selectedItems.map(item=><Cart key={item.id} data={item}/>)}
             </div>
+            {
+                state.itemsCounter > 0 && <div>
+                    <button onClick={()=>dispatch({type:"CHECKOUT"})}>Check out</button>
+                    <button onClick={()=>dispatch({type:"CLEAR"})}>Clear</button>
+                </div>
+            }
+            {
+              state.chechout && <div>
+                <h3>Check out Successfully</h3>
+                <Link to="/products">Buy More</Link>
+            </div>
+            }
+             {
+              !state.chechout && state.itemsCounter === 0 && <div>
+                <h3>Want to Buy?</h3>
+                <Link to="/products">Go to Shop</Link>
+            </div>
+            }
         </div>
     );
 };
