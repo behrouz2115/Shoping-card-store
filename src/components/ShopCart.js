78a7dbf5569dae24ab564bc5667730cd28f3f3a3
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2022-06-19 23:26:28 +0900

    ShopCart styles are added

--- src/components/ShopCart.js
+++ src/components/ShopCart.js
@@ -7,29 +7,36 @@ import Cart from './shared/Cart';
 //context
 import { CarttContext } from '../context/CartContextProvider';
 
+//styles 
+import styles from "./ShopCart.module.css";
+
 const ShopCart = () => {
     const {state,dispatch}=useContext(CarttContext);
     return (
-        <div>
-            <div>
+        <div className={styles.container}> 
+            <div className={styles.cartContainer}>
                 {state.selectedItems.map(item=><Cart key={item.id} data={item}/>)}
             </div>
             {
-                state.itemsCounter > 0 && <div>
-                    <button onClick={()=>dispatch({type:"CHECKOUT"})}>Check out</button>
-                    <button onClick={()=>dispatch({type:"CLEAR"})}>Clear</button>
+                state.itemsCounter > 0 && <div className={styles.payments}>
+                    <p><span>Totoal Items: </span>{state.itemsCounter}</p>
+                    <p><span>Totoal Payments: </span>{state.total} $</p>
+                    <div className={styles.buttonContainer}>
+                    <button className={styles.checkout} onClick={()=>dispatch({type:"CHECKOUT"})}>Check out</button>
+                    <button className={styles.clear} onClick={()=>dispatch({type:"CLEAR"})}>Clear</button>
+                    </div>
                 </div>
             }
-            {
-              state.chechout && <div>
-                <h3>Check out Successfully</h3>
-                <Link to="/products">Buy More</Link>
+             {
+                 !state.chechout && state.itemsCounter === 0 && <div className={styles.complete}>
+                <h3>Want to Buy?</h3>
+                <Link className={styles.completeP} to="/products">Go to Shop</Link>
             </div>
             }
-             {
-              !state.chechout && state.itemsCounter === 0 && <div>
-                <h3>Want to Buy?</h3>
-                <Link to="/products">Go to Shop</Link>
+            {
+              state.chechout && <div className={styles.complete}>
+                <h3>Check out Successfully</h3>
+                <Link className={styles.completeP}  to="/products"> Buy More</Link>
             </div>
             }
         </div>
