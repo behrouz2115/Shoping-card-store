849e5b24e22425f338c65d98e4ad22f7c5c7f26a
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2022-11-17 23:35:26 +0900

    demo test

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename="/Shoping-card-store">
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
